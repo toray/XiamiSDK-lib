4613db6ab72f66363ead4731d4813a058c48b4ad
bigtooth <bigbigh@gmail.com>
2014-11-21 18:03:27 +0800

    do refactor

--- src/com/toraysoft/xiami/XiamiHelper.java
+++ src/com/toraysoft/xiami/XiamiHelper.java
@@ -4,22 +4,22 @@ import android.content.Context;
 
 import com.xiami.sdk.XiamiSDK;
 
-public class XiamiUtil {
+public class XiamiHelper {
 
-	static XiamiUtil instance;
+	static XiamiHelper instance;
 
     public static final String KEY = "cb8840f9f4337a741ae9978543504a1b";
     public static final String SECRET = "dc722e9d865e892814301f61fdac3d6b";
 
 	XiamiSDK mXiamiSDK;
 
-	private XiamiUtil(Context context) {
+	private XiamiHelper(Context context) {
 		mXiamiSDK = new XiamiSDK(context, KEY, SECRET);
 	}
 
-	public static XiamiUtil get(Context context) {
+	public static XiamiHelper get(Context context) {
 		if (instance == null) {
-			instance = new XiamiUtil(context);
+			instance = new XiamiHelper(context);
 		}
 		return instance;
 	}
